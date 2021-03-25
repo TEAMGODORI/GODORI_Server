8022f67815f7ae7ea6667f038581646584da35d0
김지현 <wlguslove12345@gmail.com>
2021-03-26 01:04:33 +0900

    그룹생성하기 close #6

--- GODORI/controller/groupController.js
+++ GODORI/controller/groupController.js
@@ -2,8 +2,8 @@ const util = require('../modules/util');
 const code = require('../modules/statusCode');
 const message = require('../modules/responseMessage');
 
-const { Answer, User, Comment, Question, Scrap, Category, Group, GroupSport } = require('../models');
-const { homeService, userService } = require('../service');
+const { User, Group, GroupSport, Sport, Join } = require('../models');
+const dateService = require('../service/dateService');
 
 module.exports = {
     postNewGroup: async (req, res) => {
@@ -11,13 +11,18 @@ module.exports = {
         try {
             // sport_id와 group_maker 는 다른 테이블에 저장
             const { group_name, recruit_num, is_public, intro_comment,
-            ex_cycle, ex_intensity, sport_id, date : parse_date, group_maker } = req.body;
+            ex_cycle, ex_intensity, sports, group_maker } = req.body;
 
-            if (! group_name) {
-                return res.status(code.BAD_REQUEST).send(util.fail(code.BAD_REQUEST, message.NO_GROUP_NAME));
+            // null 값 처리
+            if (!group_name || !recruit_num || !group_maker) {
+                return res.status(code.BAD_REQUEST).send(util.fail(code.BAD_REQUEST, message.NULL_VALUE));
+            } else if (is_public === null) {
+                is_public = false;
+            } else if (!ex_cycle) {
+                ex_cycle = 3;
+            } else if (!ex_intensity) {
+                ex_intensity = "중";
             }
-
-            const parse_date = dateService.formatAnswerDate(date);
 
             // 그룹 생성
             const newGroup = await Group.create({
@@ -27,32 +32,57 @@ module.exports = {
                 intro_comment: intro_comment, // 그룹 소개 코멘트
                 ex_cycle: ex_cycle, // 그룹 인증 주기
                 ex_intensity: ex_intensity, // 그룹 인증 강도
-                parse_date: parse_date, // 파싱 날짜
-                group_maker: group_maker, // 그룹 만든 사람
-            });
-
-            // 그룹 운동종목 취향
-            const newGroupSports = await GroupSport.create({
-                
             });
 
             // 방금 생성한 그룹 아이디 추출
-            const newGroupId = await Group.findOne({
+            const findNewGroup = await Group.findOne({
                 where : {
                     group_name: group_name,
                 },
                 attributes : ['id']
             });
 
-            // 그룹 생성한 사람 join
+            const groupSports = sports.split(",");
+            for (sport of groupSports) {
+
+                // 스포츠 아이디 find
+                let findSport = await Sport.findOne({
+                    where : {
+                        name: sport
+                    },
+                    attributes : ['id']
+                })
+
+                // 그룹 운동종목 취향 저장
+                let newGroupSports = await GroupSport.create({
+                    group_id: findNewGroup.id,
+                    sport_id: findSport.id
+                });
+            }
+
+            const groupMakerId = await User.findOne({
+                where : {
+                    name : group_maker,
+                },
+                attributes : ['id']
+            })
+
+            // 그룹 생성한 사람 그룹에 가입
             const groupMakerJoin = await Join.create({
-                user_id : group_maker,
-                group_id : newGroupId.id,
+                user_id : groupMakerId.id,
+                group_id : findNewGroup.id,
             });
+
+            res.status(code.OK).send(util.success(code.OK, message.CREATE_GROUP_SUCCESS, newGroup));
 
         } catch (err) {
             console.error(err);
             return res.status(code.INTERNAL_SERVER_ERROR).send(util.fail(code.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
         }
     },
+
+    getGroupList: async (req, res) => {
+
+
+    }
 }
