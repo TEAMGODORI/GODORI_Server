b14966fdb9ce6deec57e7643416bc7d9cf1c6ac7
김지현 <wlguslove12345@gmail.com>
2021-04-17 16:28:48 +0900

    70-마이페이지 가입그룹 없어도 게시물 띄우기 close #70

--- GODORI/controller/myPageController.js
+++ GODORI/controller/myPageController.js
@@ -24,19 +24,24 @@ module.exports = {
                 },
                 attributes : ['id', 'current_group_id']
             });
-            if (user.current_group_id == 0) {
-                return res.status(code.OK).send(util.success(code.OK, message.NO_SIGNEDUP_GROUP));
+
+            let join = {
+                achive_rate : 0,
+                week_count : 0
             }
 
-            const join = await Join.findOne({
-                where : {
-                    user_id : user.id,
-                    group_id : user.current_group_id
-                },
-                attributes : ['achive_rate', 'week_count'],
-                raw : true
-            });
+            if (user.current_group_id != 0) {
+                join = await Join.findOne({
+                    where : {
+                        user_id : user.id,
+                        group_id : user.current_group_id
+                    },
+                    attributes : ['achive_rate', 'week_count'],
+                    raw : true
+                });
 
+            }
+        
             const certi_list = await myPageService.formatCertiImage(user.id);
 
             if (certi_list == 0) {
